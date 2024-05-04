b858b6a465f914eaf378fd2e5c90ba26372a7b9e
beifzzz <bekknop@gmail.com>
2024-05-04 17:10:56 +0500

    add Button and Label

--- helloWorld/ViewController.swift
+++ helloWorld/ViewController.swift
@@ -1,11 +1,38 @@
 import UIKit
 
-class ViewController: UIViewController {
 
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var mainLabel: UILabel!
+    @IBOutlet weak var greetingButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        view.backgroundColor = .cyan
+        greetingButton.layer.cornerRadius = 10
+        mainLabel.textColor = .brown
     }
     
+    @IBAction func pressedButton(_ sender: UIButton) {
+        
+        mainLabel.isHidden.toggle()
+        
+        sender.setTitle(
+            mainLabel.isHidden ? "Show Greeting" : "Hide Greeting",
+            for: .normal
+        )
+        
+        
+            
+        
+//        if mainLabel.isHidden {
+//            sender.setTitle("Show Greeting", for: .normal)
+//        } else {
+//            sender.setTitle("Hide Greeting", for: .normal)
+//        }
+    }
 }
 
